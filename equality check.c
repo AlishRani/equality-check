a1d60bde03ad1071c779f4e92ed09abca8c785d8
Alish Rani <94797867+AlishRani@users.noreply.github.com>
2021-12-02 21:15:04 +0530

    Add files via upload

---
+++ equality check.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("enter the value of a\n");
+    scanf("%d",&a);
+    printf("enter the value of b\n");
+    scanf("%d",&b);
+    if(a==b){ 
+         printf("a is equal to b");
+    }
+    else{
+         printf("a is not equal to b");
+    }
+    return 0;
+}
+   
